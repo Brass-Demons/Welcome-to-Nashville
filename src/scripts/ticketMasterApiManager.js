954c53ca7a625d95ddef456dba6e5678cd2475a3
John Long <jlcomp45@gmail.com>
2019-10-21 15:14:21 -0500

    added apikeys.js to scripts folder

--- src/scripts/ticketMasterApiManager.js
+++ src/scripts/ticketMasterApiManager.js
@@ -1,3 +1,11 @@
 console.log("it works")
-const ticketMasterBaseUrl = https://app.ticketmaster.com/discovery/v2/;
+const ticketMasterBaseUrl = `https://app.ticketmaster.com/discovery/v2/`
 
+
+const API = {
+    myData: (searchAPI) => {
+        return fetch(`${ticketMasterBaseUrl}events.json?classificationName=music&dmaId=343&apikey=FgeUGHIQw12khGk9ukP4WamWvpnoeBzC`)
+.then(genres => genres.json())
+.then(parsedGenres => {
+    console.table(parsedGenres);
+})}}
