c18032f0272e40b54b7c03282d8769f596d86adc
John Long <jlcomp45@gmail.com>
2019-10-21 17:33:53 -0500

    completed console log of data from api and found path to the info needed for data boxes for project.  next step to modularize and create functions to fulfill needs

--- src/scripts/ticketMasterApiManager.js
+++ src/scripts/ticketMasterApiManager.js
@@ -1,11 +1,25 @@
-console.log("it works")
-const ticketMasterBaseUrl = `https://app.ticketmaster.com/discovery/v2/`
+console.log("it works");
+
+const ticketMasterBaseUrl = `https://app.ticketmaster.com/discovery/v2/`;
+
+ 
+// const searchTicketMaster = searchGenre => fetch(`${ticketMasterBaseUrl}events.json?classificationName=music&dmaId=343&apikey=${concertKey}&genre`)
+// .then(genre => genre.json())
+// .then(parsedGenres => {
+//     console.log("??", parsedGenres);
+// })
+// console.log("stuff", searchTicketMaster());
 
 
-const API = {
-    myData: (searchAPI) => {
-        return fetch(`${ticketMasterBaseUrl}events.json?classificationName=music&dmaId=343&apikey=FgeUGHIQw12khGk9ukP4WamWvpnoeBzC`)
-.then(genres => genres.json())
-.then(parsedGenres => {
-    console.table(parsedGenres);
-})}}
+fetch(`${ticketMasterBaseUrl}events.json?classificationName=music&city=Nashville&apikey=${concertKey}&genre`)
+    .then(genre => genre.json())
+    .then(parsedGenres => {
+    console.log(parsedGenres._embedded.events);
+    })
+// genre = parsedGenres._embedded.events[0].classifications[0].genre.name
+// address = parsedGenres._embedded.events[0]._embedded.venues[0].address
+// location = parsedGenres._embedded.events[0]._embedded.venues[0].name
+// name = parsedGenres._embedded.events[0].name
+
+
+// check your notes in notebook for pattern & think of what you're trying to accomplish
