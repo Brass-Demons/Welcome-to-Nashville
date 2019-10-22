ff62caa7b57f9616d20115d8b3da25eddd2bcaa5
Erin Polley <erinepolley@gmail.com>
2019-10-21 21:01:38 -0500

    added function to build search form and render it to the DOM

--- src/scripts/parksApiManager.js
+++ src/scripts/parksApiManager.js
@@ -1,4 +1,15 @@
 const parksApiBaseUrl = https://data.nashville.gov/resource/74d7-b74t.json 
 
-const parkSearch = searchInput => fetch(`${parksApiBaseUrl}?$$app_token=${parkKey}&q=${searchInput}`)
+const parkSearch = searchInput => fetch(`
+${parksApiBaseUrl}?${searchInput} HTTP/1.1`), {
 .then(response => response.json())
+ {
+    "Host": "Data.nashville.gov",
+    "Accept": "application/json",
+    "X-App-Token": "P7SZS21rPkPvHmuIvW4glESQf" ${parkKey}
+    
+}
+}
+
+
+$$app_token=${parkKey}?
