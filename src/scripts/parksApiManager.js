97cd23bf909c5606f215a874f3bc0f5adcc560bc
Erin Polley <erinepolley@gmail.com>
2019-10-22 15:53:34 -0500

    fixed some problems with rendering search form to DOM

--- src/scripts/parksApiManager.js
+++ src/scripts/parksApiManager.js
@@ -1,6 +1,6 @@
 const parksApiBaseUrl = "https://data.nashville.gov/resource/74d7-b74t.json" 
 
-const parkSearch = () => fetch(`
+const parkSearch = (searchInput) => fetch(`
 ${parksApiBaseUrl}?${searchInput} HTTP/1.1`,
     {
         "headers": {
--- src/scripts/parksEventManager.js
+++ src/scripts/parksEventManager.js
@@ -18,10 +18,10 @@ console.log("hi")
 
 
 
-//This function is making the search form in HTML. 
+//MAKING SEARCH FORM IN AN HTML STRING.
 
 const makeSearchForm = () => {
-    const parkSearchList = `
+  const parkSearchList = `
     <fieldset>
     <select id ="parks">
     <option value="" disabled selected hidden>parks by feature</option>
@@ -31,61 +31,65 @@ const makeSearchForm = () => {
     <option value="basketball_court">Basketball Courts</option>
     <option value="swimming_pool">Swimming Pool</option>
     </select>
-    <button id = park-search-button></button>
+    <button id="park-search-button">Button</button>
 		</fieldset>
     `
-    // console.log(searchForm)
-  //This section is taking the search form and rendering it to the DOM in the section with the id of parks.
-    const parkSearchContainerDiv = document.querySelector("");
-    parkSearchContainerDiv.innerHTML = parkSearchList;
-  };
+  // console.log(searchForm)
+  //Taking the search form and rendering it to the DOM
+  const parkSearchContainerDiv = document.querySelector("#search");
+  parkSearchContainerDiv.innerHTML = parkSearchList;
+};
 //This will go on the main JS page in the end.
-  makeSearchForm();
+makeSearchForm();
 
-  //SEARCH RESULTS MANAGER .JS FILE
-  //I need the park_name, human_address
-  //human_address IS AN OBJECT. But that is okay, because I can target the thingy 
+//Grabbing whatever is in the option tag of the dropdown menu, putting the value of that in the searchInput variable
+const searchHandler = () => {
+  debugger
+  const parkSearchInput = document.querySelector("option")
+  console.log("user input from dropdown", parkSearchInput.value);
+  //parkSearch is the function that fetches the data.
+  parkSearch(parkSearchInput.value)
+    .then(response => {
+      console.log(response.parks)
+      searchInput.value = ""
+    })
+};
 
-  const parkResultsToHtml = (parkResult) => {
-`
+// const attachEventListenerToSearchButton = () => {
+  let searchButton = document.querySelector("#park-search-button")
+  console.log(searchButton)
+  searchButton.addEventListener("click", searchHandler);
+//SEARCH RESULTS MANAGER .JS FILE
+//I need the park_name, human_address
+//human_address IS AN OBJECT. But that is okay, because I can target the thingy 
+
+const parkResultsToHtml = (parkResult) => {
+  `
   <li>${parkResult.park_name}: ${parkResult.mapped_location.human_address.address}<button id="parks-button">Save</button></li>
 `
-  };
+};
 //This function does the following:
 //1. Grabs the container on HTML that the search results will go in
 //2. iterates over each search result and passes them through the parkRestultsToHtml function to convert it to a string with interpolation
 //3. Comes back and sticks the string in that ol. 
-  const parkDisplay = (parkSearchResults) => {
-    let parksHtml = ""
-    const parkResultsOnDom = document.querySelector("ol")
-    parkSearchResults.forEach(park => {
-     parksHtml += parkResultsToHtml(park)
+const parkDisplay = (parkSearchResults) => {
+  let parksHtml = ""
+  const parkResultsOnDom = document.querySelector("ol")
+  parkSearchResults.forEach(park => {
+    parksHtml += parkResultsToHtml(park)
     parkResultsOnDom.innerHTML = parksHtml;
-    });
+  });
 
-const saveThisParkButton = () => {
-  const buttonStuff = document.querySelector("#parks-button")
-};
-//This is the event listener for the "SAVE" button.
-//The function needs to take that selected park and stick it in the DOM on the <li>Park:</li> part of the page.
-const whenSaveButtonIsClicked = () => {
-  const saveButton = document.querySelector("button")
-  saveButton.addEventListener("click", saveThisParkButton());
-}; 
-  }
-//Grabbing whatever is in the option tag of the dropdown menu, putting the value of that in the searchInput variable
-const search = event => {
-const searchInput = document.querySelector("option")
-console.log("user input from dropdown", searchInput.value);
-//parkSearch is the function that fetches the data.
-parkSearch(searchInput.value)
-.then(response => {
-console.log(response.parks)
-searchInput.value = ""
-})
+  //FUNCITON FOR EVENT LISTENER ON SAVE BUTTON
+  const saveThisParkButton = () => {
+    const buttonStuff = document.querySelector("#parks-button")
+  };
+  //This is the event listener for the "SAVE" button.
+  //The function needs to take that selected park and stick it in the DOM on the <li>Park:</li> part of the page.
+  const whenSaveButtonIsClicked = () => {
+    const saveButton = document.querySelector("button")
+    saveButton.addEventListener("click", saveThisParkButton);
+  };
 }
 
-const attachEventListenerToSearchButton = () => {
-  const searchButton = document.querySelector("park-search-button")
-  searchButton.addEventListener("click", search)
-}
+
