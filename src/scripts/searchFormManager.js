0bbdbf35ef72128d6296e44e63e117aaf3d639dd
Melody Stern <melliemuse@gmail.com>
2019-10-22 15:52:30 -0500

    cleaned out contents of html and added dynamically to js files

--- src/scripts/searchFormManager.js
+++ src/scripts/searchFormManager.js
@@ -1,7 +1,13 @@
 // Defines function which creates and appends search form HTML to the DOM
 
 const searchFormHTML = () => {
-	return `<fieldset>
+	return `
+	<h1>Welcome to Nashville</h1>
+	</section>
+	<section >
+		<h3>Search for stuff to do today</h3>
+	</section>
+	<fieldset>
 		<input type="text" name="meetups" placeholder="meetups by topic">
 		<button class='meetup__searchButton'>SEARCH</button>
 	</fieldset>`
