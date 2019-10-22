cc19f586f5d76ad20277c78ff1b33f088e5a1b4f
Erin Polley <erinepolley@gmail.com>
2019-10-21 21:21:31 -0500

    changed index.html to select instead of input type=text. Added 'parks by feature' to dropdown menu

--- src/scripts/parksEventManager.js
+++ src/scripts/parksEventManager.js
@@ -1,17 +1,20 @@
+console.log("hi")
+
 //This function is making the search form in HTML. 
-const buildAndAppendSearchForm = () => {
+const makeSearchForm = () => {
     const searchForm = `
-    <select name="park-feature" id="park-feature">
+    <option value="" disabled selected hidden>parks by feature</option>
     <option value="dog-park">Dog Park</option>
     <option value="hiking">Hiking Trails</option>
     <option value="playground">Playground</option>
     <option value="basketball">Basketball Courts</option>
     <option value="swimming-pool">Swimming Pool</option>
-</select>
+
     `
     // console.log(searchForm)
-  //This section is taking the search form and rendering it to the DOM in a section with the id of container. . 
+  //This section is taking the search form and rendering it to the DOM in a section with the id of parks.
     const parkContainerDiv = document.querySelector("#parks")
-    parkContainerDiv.innerHTML = searchForm
+    parkContainerDiv.innerHTML = searchForm;
   }
 
+  makeSearchForm()
