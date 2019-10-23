58049f8e1177de8fef3c6d6b37602c4a4d60799a
John Long <jlcomp45@gmail.com>
2019-10-23 10:49:17 -0500

    corrected code for search button to populate results section and can now search by genre with different results returning.  search results gives name, location, and address

--- src/scripts/ticketMasterApiManager.js
+++ src/scripts/ticketMasterApiManager.js
@@ -9,58 +9,59 @@ const ticketMasterBaseUrl = `https://app.ticketmaster.com/discovery/v2/`;
 // console.log("stuff", myParsedGenres);
 const tmAPI = {
     myParsedGenres: (inputValue) => {
-        return fetch(`${ticketMasterBaseUrl}events.json?classificationName=music&dmaId=343&apikey=${concertKey}&genre=${inputValue}`)
+        return fetch(`${ticketMasterBaseUrl}events.json?classificationName=${inputValue}&dmaId=343&apikey=${concertKey}`)
             .then(genre => genre.json())
+    }
 }
 const handleSearch = event => {
-    const inputField = document.getElementById("concertInput");
-    tmAPI.myParsedGenres(inputField.value)
-        .then(response => {
-            buildResultsForm(response._embedded.events)
-        })
-}
+        const inputField = document.getElementById("concertInput");
+        tmAPI.myParsedGenres(inputField.value)
+            .then(response => {
+                buildResultsForm(response._embedded.events)
+            })
+    }
 
 
 const buildSearchForm = () => {
-    const genreSearchForm = `
+        const genreSearchForm = `
     <fieldset>
 			<input type="text" id="concertInput" placeholder="concerts by genre">
 			<button class="genreButton">Search Genre</button>
         </fieldset>
         `
 
-    const genreContainerDiv = document.querySelector(".genreSearch");
-    genreContainerDiv.innerHTML += genreSearchForm;
-}
+        const genreContainerDiv = document.querySelector(".genreSearch");
+        genreContainerDiv.innerHTML += genreSearchForm;
+    }
 buildSearchForm();
 const handleGenreSearch = event => {
-    const genreInputField = document.getElementById("concertInput");
-    tmAPI.myParsedGenres(genreInputField.value)
-        .then(parsedGenres => {
-            buildResultsForm(parsedGenres._embedded.events);
-            genreInputField.value = "";
-        })
-}
+        const genreInputField = document.getElementById("concertInput");
+        tmAPI.myParsedGenres(genreInputField.value)
+            .then(parsedGenres => {
+                buildResultsForm(parsedGenres._embedded.events);
+                genreInputField.value = "";
+            })
+    }
 const eventListenerToGenreSearch = () => {
-    const genreSearchButton = document.querySelector(".genreButton");
-    genreSearchButton.addEventListener("click", handleSearch);
-}
+        const genreSearchButton = document.querySelector(".genreButton");
+        genreSearchButton.addEventListener("click", handleSearch);
+    }
 
 // code above this line is clean
 
 
 const buildResultsForm = (genreArray) => {
-    console.log("help", genreArray);
-    genreArray.forEach(genre => {
-        console.log("what's here?", genre);
-        const genreResultsList = `
-        <li>${genre.name}<button class="genreSave">Save</button></li>
+        console.log("help", genreArray);
+        genreArray.forEach(genre => {
+            console.log("what's here?", genre._embedded.venues[0].name);
+            const genreResultsList = `
+        <li>${genre.name}: ${genre._embedded.venues[0].name}: ${genre._embedded.venues[0].address.line1} <button class="genreSave">Save</button></li>
         `
-        const genreResults = document.querySelector(".genreResult");
-        genreResults.innerHTML += genreResultsList;
-        console.log("test", genreResultsList)
+            const genreResults = document.querySelector(".genreResult");
+            genreResults.innerHTML += genreResultsList;
+            console.log("test", genreResultsList)
+        }
+        )
     }
-    )
-}
 eventListenerToGenreSearch();
 
