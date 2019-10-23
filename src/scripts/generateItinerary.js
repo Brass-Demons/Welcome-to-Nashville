67950b07a7f319bda74bde55e3108831c4b82c6a
Melody Stern <melliemuse@gmail.com>
2019-10-23 09:56:38 -0500

    added object to contain all saved search results and added their values to html-creating funciton using string interpolation

--- src/scripts/generateItinerary.js
+++ src/scripts/generateItinerary.js
@@ -1,17 +1,27 @@
 // Create HTML and add to DOM for Itinerary
 
+const itineraryObj = {
+    park : ,
+    restaurant: ,
+    meetup: ,
+    concert:
+};
 
 const displayItinerary = () => {
-    const itineraryHTML = 
-    `<h3>My Itinerary</h3>
-    <ul>
-    <li>Park: </li>
-    <li>Restaurant: </li>
-    <li>Meetup: </li>
-    <li>Concert: </li>
-    </ul>`
-    
-    const itineraryContainer = document.querySelector('.container__itinerary')
-    itineraryContainer.innerHTML = itineraryHTML
-}
 
+    // same as writing const park = itinerary.park, const restaurant = itinerary.restaurant, etc.
+    const { park, restaurant, meetup, concert } = itineraryObj;
+
+    const itineraryHTML = `
+        <h3>My Itinerary</h3>
+        <ul>
+            <li>Park: ${park}</li>
+            <li>Restaurant: ${restaurant}</li>
+            <li>Meetup: ${meetup}</li>
+            <li>Concert: ${concert}</li>
+        </ul>
+    `;
+    
+    const itineraryContainer = document.querySelector('.container__itinerary');
+    itineraryContainer.innerHTML = itineraryHTML;
+};
