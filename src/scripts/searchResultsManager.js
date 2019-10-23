218f216418a90b0696318f4e7d05489d664505b5
Melody Stern <melliemuse@gmail.com>
2019-10-23 14:40:44 -0500

    adding ids to save buttons

--- src/scripts/searchResultsManager.js
+++ src/scripts/searchResultsManager.js
@@ -9,9 +9,9 @@ const buildResultsHTML = () => {
     let resultsHTML = 	``
     searchResults.forEach(function (event) {
         
-        // console.log(event);
+        console.log(event.id);
         if (event) {
-            resultsHTML += `<li>${event.name.text}: ${event.description.text}<button class='save'>Save</button></li>`;
+            resultsHTML += `<li>${event.name.text}: ${event.description.text}<button class= 'save${event.id}'>Save</button></li>`;
 
         } else {
             resultsHTML += `<li> No results found <button class='save'>Save</button></li>`;
@@ -26,10 +26,18 @@ const buildResultsHTML = () => {
     ${resultsHTML}
     </ol>` 
 
+
     const saveButtons = document.querySelectorAll('.save');
-    saveButtons.forEach(function(saveButton) {
-        saveButton.addEventListener('click', displayItinerary)
-    })
+    for (let i = 0; i < saveButtons.length; i++) {
+        // saveButtons[i].className = `save__meetup${[i]}`
+        saveButtons[i].addEventListener('click', displayItinerary)
+        console.log(saveButtons)
+    }
+
+    // const saveButtons = document.querySelectorAll('.save');
+    // saveButtons.forEach(function(saveButton) {
+    //     saveButton.addEventListener('click', displayItinerary)
+    // })
     console.log(saveButtons)
 }
 
