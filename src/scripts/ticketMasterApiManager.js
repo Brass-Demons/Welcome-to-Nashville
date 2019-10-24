7a683c7a682450cc1c6ec0e474bd11714597aee0
John Long <jlcomp45@gmail.com>
2019-10-24 07:42:50 -0500

    basic html render for search fields compatible with melody code, and event listener correctly working on search button rendering to dom still with my function.  next branch will be to correct that so it renders from team function melody created

--- src/scripts/ticketMasterApiManager.js
+++ src/scripts/ticketMasterApiManager.js
@@ -1,4 +1,5 @@
 console.log("it works");
+debugger
 const ticketMasterBaseUrl = `https://app.ticketmaster.com/discovery/v2/`;
 
 // initial fetch parsing by genre
@@ -11,8 +12,7 @@ const tmAPI = {
 
 // function to take the search event input info and return the data in a usable form
 const handleSearch = event => {
-    console.log("stuff2");
-    const inputField = document.getElementById("concertInput");
+    const inputField = document.querySelector(".concertInput");
     tmAPI.myParsedGenres(inputField.value)
     .then(response => {
         buildResultsForm(response._embedded.events)
@@ -20,24 +20,24 @@ const handleSearch = event => {
 }
 
 // function to build the search box and button and populate to the dom
-const buildSearchForm = () => {
-    const genreSearchForm = `
-    <fieldset>
-    <input type="text" id="concertInput" placeholder="concerts by genre">
-    <button class="genreButton">Search Genre</button>
-    </fieldset>
-    `
+// const buildSearchForm = () => {
+//     const genreSearchForm = `
+//     <fieldset>
+//     <input type="text" id="concertInput" placeholder="concerts by genre">
+//     <button class="genreButton">Search Genre</button>
+//     </fieldset>
+//     `
     
-    const genreContainerDiv = document.querySelector(".container__searchFields");
-    genreContainerDiv.innerHTML += genreSearchForm;
-}
+//     const genreContainerDiv = document.querySelector(".container__searchFields");
+//     genreContainerDiv.innerHTML += genreSearchForm;
+// }
 
 // calling function to display to dom
-buildSearchForm();
+// buildSearchForm();
 
 // function to parse info from json object to only what user asked for in terms of genre
 const handleGenreSearch = event => {
-    const genreInputField = document.getElementById("concertInput");
+    const genreInputField = document.querySelector(".concertInput");
     tmAPI.myParsedGenres(genreInputField.value)
     .then(parsedGenres => {
         buildResultsForm(parsedGenres._embedded.events);
@@ -47,17 +47,18 @@ const handleGenreSearch = event => {
 
 // adding event listener to search button to display results to dom, calls handleSearch function
 const eventListenerToGenreSearch = () => {
-    const genreSearchButton = document.querySelector(".genreButton");
+    const genreSearchButton = document.querySelector(".concerts__searchButton");
+    console.log("test test", genreSearchButton);
     genreSearchButton.addEventListener("click", handleSearch);
 }
 // calling event listener function
-eventListenerToGenreSearch();
+// eventListenerToGenreSearch();
 
 // code above this line is clean
 
 // function to populate dom with results from search field
 const buildResultsForm = (genreArray) => {
-    console.log("help", genreArray);
+    // console.log("help", genreArray);
     genreArray.forEach(genre => {
         console.log("what's here?", genre._embedded.venues[0].name);
         const genreResultsList = `
@@ -65,7 +66,7 @@ const buildResultsForm = (genreArray) => {
         `
         const genreResults = document.querySelector(".container__results");
         genreResults.innerHTML += genreResultsList;
-        console.log("test", genreResultsList)
+        // console.log("test", genreResultsList)
     }
     )
 }
