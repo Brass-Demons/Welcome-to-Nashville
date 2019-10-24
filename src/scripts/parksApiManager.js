f351de3498215af07c3ff794009c03b8792e88f9
Erin Polley <erinepolley@gmail.com>
2019-10-24 09:43:40 -0500

    made some changes to functions to make them work better. Now search results rendered to the DOM.

--- src/scripts/parksApiManager.js
+++ src/scripts/parksApiManager.js
@@ -1,7 +1,7 @@
 const parksApiBaseUrl = "https://data.nashville.gov/resource/74d7-b74t.json" 
 
-const parkSearch = (searchInput) => fetch(`
-${parksApiBaseUrl}?${searchInput} `,
+const parkSearch = (parkSearchInput) => fetch(`
+${parksApiBaseUrl}?${parkSearchInput}=Yes`,
     {
         "headers": {
             "Accept": "application/json",
--- src/scripts/parksEventManager.js
+++ src/scripts/parksEventManager.js
@@ -1,4 +1,5 @@
 console.log("hi")
+// debugger
 //Order of JS files in Jisie's code:
 //1. api keys--done. IN API KEYS FILE.
 
@@ -28,7 +29,7 @@ const makeSearchForm = () => {
     <option value="dog_park">Dog Park</option>
     <option value="hiking_trails">Hiking Trails</option>
     <option value="playground">Playground</option>
-    <option value="basketball_court">Basketball Courts</option>
+    <option value="basketball_courts">Basketball Courts</option>
     <option value="swimming_pool">Swimming Pool</option>
     </select>
     <button id="parks-search-button">Button</button>
@@ -36,71 +37,65 @@ const makeSearchForm = () => {
     `
   // console.log(searchForm)
   //Taking the search form and rendering it to the DOM
-  const parkSearchContainerDiv = document.querySelector("container__searchFields");
+  const parkSearchContainerDiv = document.querySelector(".container__searchFields");
   parkSearchContainerDiv.innerHTML = parkSearchList;
 };
 //This will go on the main JS page in the end.
 makeSearchForm();
 
-//I need to grab the id of whichever feature they pick from dropdown menu.
-// var searchOptionValue = document.getElementById("parks").selectedOptions[0].value
-// console.log(searchOptionValue)
-
 const searchHandler = () => {
   const parkSearchInput = document.getElementById("parks-dropdown").value
-  console.log(e.value);
+  console.log("SEARCH VALUE", parkSearchInput);
   //parkSearch is the function that fetches the data.
 
-  parkSearch(parkSearchInput.value)
-    .then(response => {
-      parkDisplay(response.parks)
-      //Not sure why this is here in Jisie's code
-      searchInput.value = ""
-    })
-};
-
-// const attachEventListenerToSearchButton = () => {
-let searchButton = document.querySelector("#parks-search-button")
-console.log(searchButton)
-searchButton.addEventListener("click", searchHandler);
-//SEARCH RESULTS MANAGER .JS FILE
-//I need the park_name, human_address
-//human_address IS AN OBJECT. But that is okay, because I can target the thingy 
-
-const parkResultsToHtml = (parkResult) => {
-  `
-  <li id = "${parkResult.park_name}">${parkResult.park_name}: ${parkResult.mapped_location.human_address.address}<button id="parks-save-button">Save</button></li>
-`
-};
-//This function does the following:
-//1. Grabs the container on HTML that the search results will go in
-//2. iterates over each search result and passes them through the parkRestultsToHtml function to convert it to a string with interpolation
-//3. Comes back and sticks the string in that ol. 
-const parkDisplay = (parkSearchResults) => {
-  let parksHtml = ""
-  const parkResultsOnDom = document.querySelector(".container__results")
-  parkSearchResults.forEach(park => {
-    parksHtml += parkResultsToHtml(park)
-    parkResultsOnDom.innerHTML = parksHtml;
-  });
-
-  //FUNCTION FOR EVENT LISTENER ON SAVE BUTTON
-  //This needs to take whichever save button is clicked, and take that stuff and put it in the li of the ininerary list
+  parkSearch(parkSearchInput)
+    .then(response => parkDisplay(response))
+}
 
 
+  // const attachEventListenerToSearchButton = () => {
+    let searchButton = document.querySelector("#parks-search-button")
+    // console.log(searchButton)
+    searchButton.addEventListener("click", searchHandler);
+    //SEARCH RESULTS MANAGER .JS FILE
+    //I need the park_name, human_address
+    //human_address IS AN OBJECT. But that is okay, because I can target the thingy 
+  // }
+
+
+
+  const parkResultsToHtml = (parkResult) => {
+  parkResult.mapped_location.split()
+    console.log(parkResult.mapped_location)
+    return `<ul>
+  <li id = "${parkResult.park_name}">${parkResult.park_name}: ${parkResult.mapped_location.human_address}<button id="${parkResult.park_name}">Save</button></li></ul>`;
+
+  };
+  //This function does the following:
+  //1. Grabs the container on HTML that the search results will go in
+  //2. iterates over each search result and passes them through the parkRestultsToHtml function to convert it to a string with interpolation
+  //3. Comes back and sticks the string in that ol. 
+  const parkDisplay = (parkSearchResultsArray) => {
+    let parksHtml = ""
+    const parkResultsOnDom = document.querySelector(".container__results")
+    parkSearchResultsArray.forEach(park => {
+      parksHtml += parkResultsToHtml(park)
+      parkResultsOnDom.innerHTML = parksHtml;
+      // attachEventListenerToSaveButton(park)
+    });
+
+    //FUNCTION FOR EVENT LISTENER ON SAVE BUTTON
+    //This needs to take whichever save button is clicked, and take that stuff and put it in the li of the ininerary list
   }
 
-  const saveThisParkButton = () => {
-    const itineraryContainer = document.querySelector("ul")
-    itineraryContainer.innerHTML = `<li id = "${parkResult.park_name}">${parkResult.park_name}
-    `
-  };
+  // const saveThisParkButton = () => {
+  //   const itineraryContainer = document.querySelector(`${["address"]}`)
+  //   itineraryContainer.innerHTML = `<li id = "${parkResult.park_name}">${parkResult.park_name}
+  //   `
+  // };
   //This is the event listener for the "SAVE" button.
   //The function needs to take that selected park and stick it in the DOM on the <li>Park:</li> part of the page.
-  // const whenSaveButtonIsClicked = () => {
-    const saveButton = document.querySelector("#parks-save-button")
-    saveButton.addEventListener("click", saveThisParkButton);
-  
-
-
-
+  const whenSaveButtonIsClicked = () => {
+  const saveButton = document.querySelector("#parks-save-button")
+  saveButton.addEventListener("click", saveThisParkButton)
+  }
